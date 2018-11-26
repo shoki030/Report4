1eb6771713d9833bfc813d07c9a42e64b0d3b04d
Shoki Yamamoto <e185711@ie.u-ryukyu.ac.jp>
2018-11-26 23:00:25 +0900

    3rd commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,6 @@
 public class Main {
-    public static void main(String[] args) {
-        try {
-            String str = null;
-            System.out.println(str.length());
-        }
-        catch (NullPointerException e){
-            System.out.println(e + "が発生。");
-            System.out.println(e.getMessage());
-
-        }
-    }
+   public static void main(String[] args){
+       String str = "百二十三";
+       Integer.parseInt(str);
+   }
 }
